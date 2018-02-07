d1d262bd1094be5f7122babd7f90c0bf06458f5f
ANTHONY ANDONIELLO <SublethalRex@DESKTOP-39CRHCC>
2018-02-06 23:35:49 -0700

    jumble with added jumbletron

--- keywords/keywords.cpp
+++ keywords/keywords.cpp
@@ -4,7 +4,8 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -20,7 +21,15 @@ int main()
 
 	}
 
-
+	string jumble = keywords[1];
+	int length = jumble.size();
+	for (int i = 0; i < length; ++i) {
+		int index1 = (rand() % length);
+		int index2 = (rand() % length);
+		char temp = jumble[index1];
+		jumble[index1] = jumble[index2];
+		jumble[index2] = temp;
+	}
 
 
 	cin.get();
